b5c1aba0a18090a6ccab4b8290ea9adebbaa4d2a
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2026-02-23 13:22:13 -0500

    add in auto server run gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("sh.harold.hytale.run") version "0.1.1"
+    }
+}
+
 rootProject.name = "blackbox"
 
 include(":blackbox-core")
